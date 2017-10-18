90b4e2a79a6ac80760cfc999f6e56c3cb8eb5068
tommoyang <tommoyang@hotmail.com>
2017-10-18 21:35:42 +1300

    Cleanup

--- src/content/script.js
+++ src/content/script.js
@@ -58,54 +58,77 @@
     });
 
     function pageLoad(url) {
+        var response;
         if (url.indexOf('#guild') !== -1) {
-            if ($('.prt-assault-guildinfo').length > 0) {
-                var times = [];
-                $('.prt-assault-guildinfo').find('.prt-item-status').each(function (index) {
-                    var text = $(this).text();
-                    var hour = parseInt(text.split(':')[0]);
-                    if (text.indexOf('p.m.') !== -1 && text.indexOf('p.m') < text.length - 5) {
-                        if (hour !== 12) {
-                            hour += 12;
-                        }
-                    } else if (hour === 12) {
-                        hour = 0;
-                    }
-                    times[index] = hour;
-                });
-                messageDevTools({assault: {'times': times}});
-            }
+            response = findInfoGuild();
+            messageDevTools(response);
         } else if (url.indexOf('#mypage') !== -1) {
-            var $prtUserInfo = $('.prt-user-info');
-            var $prtInfoStatus = $prtUserInfo.children('.prt-info-status');
-            var $prtInfoPossessed = $prtUserInfo.children('.prt-info-possessed');
-            var $prtMbpPossessed = $prtUserInfo.children('#mbp-status');
-            messageDevTools({
-                profile: {
-                    'rank': $prtInfoStatus.find('.txt-rank-value').attr('title'),
-                    'rankPercent': $prtInfoStatus.find('.prt-rank-gauge-inner').attr('style'),
-                    'job': $prtInfoStatus.find('.txt-joblv-value').attr('title'),
-                    'jobPercent': $prtInfoStatus.find('.prt-job-gauge-inner').attr('style'),
-                    'jobPoints': $prtInfoPossessed.eq(1).find('.prt-jp').text(),
-                    'renown': $prtMbpPossessed.find('.txt-current-point').eq(0).text(),
-                    'prestige': $prtMbpPossessed.find('.txt-current-point').eq(1).text()//$(this).text()
+            response = findInfoMypage();
+            messageDevTools(response);
+        } else if (url.indexOf('#coopraid/room/') !== -1) {
+            response = findInfoCoop();
+            messageDevTools(response);
+        } else if (url.indexOf('#casino') !== -1) {
+            response = findInfoCasino();
+            messageDevTools(response);
+        }
+    }
+
+    function findInfoGuild() {
+        var $guildInfo = $('.prt-assault-guildinfo');
+
+        if ($guildInfo.length > 0) {
+            var times = [];
+            $guildInfo.find('.prt-item-status').each(function (index) {
+                var text = $(this).text();
+                var hour = parseInt(text.split(':')[0]);
+                if (text.indexOf('p.m.') !== -1 && text.indexOf('p.m') < text.length - 5) {
+                    if (hour !== 12) {
+                        hour += 12;
+                    }
+                } else if (hour === 12) {
+                    hour = 0;
+                }
+                times[index] = hour;
+            });
+            return ({assault: {'times': times}});
+        }
+    }
+
+    function findInfoMypage() {
+        var $prtUserInfo = $('.prt-user-info');
+        var $prtInfoStatus = $prtUserInfo.children('.prt-info-status');
+        var $prtInfoPossessed = $prtUserInfo.children('.prt-info-possessed');
+        var $prtMbpPossessed = $prtUserInfo.children('#mbp-status');
+
+        return ({
+            profile: {
+                'rank': $prtInfoStatus.find('.txt-rank-value').attr('title'),
+                'rankPercent': $prtInfoStatus.find('.prt-rank-gauge-inner').attr('style'),
+                'job': $prtInfoStatus.find('.txt-joblv-value').attr('title'),
+                'jobPercent': $prtInfoStatus.find('.prt-job-gauge-inner').attr('style'),
+                'jobPoints': $prtInfoPossessed.eq(1).find('.prt-jp').text(),
+                'renown': $prtMbpPossessed.find('.txt-current-point').eq(0).text(),
+                'prestige': $prtMbpPossessed.find('.txt-current-point').eq(1).text()//$(this).text()
+            }
+        });
+    }
+
+    function findInfoCoop() {
+        return ({
+            coopCode:
+                $('.txt-room-id').eq(0).text()
+        });
+    }
+
+    function findInfoCasino() {
+        var amt = parseInt($('.prt-having-medal').children('.txt-value').first().attr('value'));
+        if (!isNaN(amt)) {
+            return ({
+                chips: {
+                    'amount': amt
                 }
             });
-        } else if (url.indexOf('#coopraid/room/') !== -1) {
-            messageDevTools({
-                coopCode:
-                    $('.txt-room-id').eq(0).text()
-            });
-
-        } else if (url.indexOf('#casino') !== -1) {
-            var amt = parseInt($('.prt-having-medal').children('.txt-value').first().attr('value'));
-            if (!isNaN(amt)) {
-                messageDevTools({
-                    chips: {
-                        'amount': amt
-                    }
-                });
-            }
         }
     }
 
