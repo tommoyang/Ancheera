f54e79d50da3266aa517374ea3781b63af55ceec
Mattish <mattishd@gmail.com>
2017-10-22 00:01:08 +0100

    moved bp to its own tracker(too)

--- src/background/apbp.js
+++ src/background/apbp.js
@@ -1,19 +1,6 @@
 (function () {
     var apTracker = new ApTracker();
-
-    var currBP = 0;
-    var maxBP = 0;
-
-    var bpTime = {
-        hour: 0,
-        minute: 0,
-        second: 0
-    };
-
-    var bpTimer;
-    var availableRaids = {};
-    var currRaids = {};
-    var decBP = 0;
+    var bpTracker = new BpTracker();
 
     window.APBP = {
         VerifyAPBP: function (json) {
@@ -31,36 +18,12 @@
             }
             if (status !== undefined) {
                 apTracker.updateFromJson(status);
-                setBP(status.bp, status.max_battle_point);
-                if (status.battle_point_remain.indexOf('00:00') === -1) {
-                    index = status.battle_point_remain.indexOf('h');
-                    hour = bpTime.hour;
-                    minute = bpTime.minute;
-                    if (index !== -1) {
-                        bpTime.hour = Number(status.battle_point_remain.substring(0, index));
-                    } else {
-                        bpTime.hour = 0;
-                    }
-                    if (status.battle_point_remain.indexOf('m') !== -1) {
-                        bpTime.minute = Number(status.battle_point_remain.substring(index + 1, status.battle_point_remain.length - 1));
-                    } else {
-                        bpTime.minute = 0;
-                    }
-                    if (hour !== bpTime.hour || minute !== bpTime.minute) {
-                        bpTime.second = 59;
-                        setBPTime();
-                        resetBPTimer();
-                    }
-                } else {
-                    stopBPTimer();
-                }
+                bpTracker.updateFromJson(status);
             }
         },
-
         InitializeQuest: function (json) {
             apTracker.stageQuest(json.action_point);
         },
-
         StartQuest: function (json, payload) {
             var id = payload.quest_id;
             if (id == 715571 || id == 715561 || id == 715551) {
@@ -79,32 +42,15 @@
                 apTracker.stageQuest(0);
             }
         },
-
         InitializeRaid: function (json) {
-            availableRaids = {};
-            var raid;
-            for (var i = 0; i < json.assist_raids_data.length; i++) {
-                raid = json.assist_raids_data[i];
-                availableRaids[raid.raid.id] = {
-                    bp: raid.used_battle_point
-                };
-            }
+            bpTracker.stageRaidFromJson(json);
         },
         InitializeRaidCode: function (json) {
-            availableRaids = {};
-            availableRaids[json.raid.id] = {
-                bp: json.used_battle_point
-            };
+            bpTracker.stageRaidCodeFromJson(json);
         },
 
         StartRaid: function (json, payload) {
-            if (json.result !== false) {
-                if (json.is_host === false) {
-                    spendBP(parseInt(payload.select_bp));
-                }
-                currRaids[json.raid_id] = availableRaids[json.raid_id];
-            }
-            availableRaids = {};
+            bpTracker.spendBpOnStagedRaid(json, payload)
             apTracker.stageQuest(0);
         },
         RestoreAPBP: function (json) {
@@ -112,7 +58,7 @@
                 if (json.result.recovery_str === "AP") {
                     apTracker.addAp(json.result.after - json.result.before);
                 } else if (json.result.recovery_str === "EP") {
-                    apTracker.addAp(json.result.after - json.result.before);
+                    bpTracker.addBp(json.result.after - json.result.before);
                 }
             }
         },
@@ -121,7 +67,7 @@
         },
         SetMax: function () {
             apTracker.addAp(apTracker.getMaxAp());
-            addBP(maxBP);
+            bkTracker.addBP(bpTracker.getMaxBp());
         }
     }
 
@@ -129,71 +75,16 @@
         apTracker.addAp(amount);
     }
 
-    var spendBP = function (amt) {
-        var full = false;
-        if (currBP >= maxBP) {
-            full = true;
-            setBP(currBP - amt, maxBP);
-            if (currBP < maxBP) {
-                amt = maxBP - currBP;
-            } else {
-                return;
-            }
-        } else {
-            setBP(currBP - amt, maxBP);
-        }
-        if (currBP < maxBP) {
-            bpTime.minute += amt * 10 % 60;
-            if (bpTime.minute >= 60) {
-                bpTime.minute -= 60;
-                bpTime.hour++;
-            }
-            if (full) {
-                bpTime.minute--;
-                if (bpTime.minute < 0) {
-                    bpTime.minute += 60;
-                    bpTime.hour--;
-                }
-                bpTime.second = 59;
-            }
-            bpTime.hour += Math.floor(amt * 10 / 60);
-            setBPTime();
-            if (!bpTimer) {
-                resetBPTimer();
-            }
-        }
+    var spendBP = function (amount) {
+        bpTracker.spendBp(amount);
     }
 
-    var setBP = function (curr, max) {
-        currBP = parseInt(curr);
-        maxBP = parseInt(max);
-        Message.PostAll({
-            setText: {
-                'id': '#bp-number',
-                'value': 'EP: ' + currBP + '/' + maxBP
-            }
-        });
-        Message.PostAll({
-            setBar: {
-                'id': '#bp-bar',
-                'value': ((currBP / maxBP) * 100) + '%'
-            }
-        });
+    var setBP = function (current, max) {
+        bpTracker.setBp(current, max);
     }
 
-    var addBP = function (amt) {
-        setBP(currBP + amt, maxBP);
-        if (currBP >= maxBP) {
-            stopBPTimer();
-        } else {
-            bpTime.minute -= amt * 10 % 60;
-            if (bpTime.minute < 0) {
-                bpTime.minute += 60;
-                bpTime.hour--;
-            }
-            bpTime.hour -= Math.floor(amt * 10 / 60);
-            setBPTime();
-        }
+    var addBP = function (amount) {
+        bpTracker.addBp(amount);
     }
 
     var setAP = function (curr, max) {
@@ -204,62 +95,6 @@
         apTracker.setApTime();
     }
     var setBPTime = function () {
-        var str = "";
-        if (bpTime.hour > 0) {
-            str += bpTime.hour + ':';
-            if (bpTime.minute < 10) {
-                str += '0'
-            }
-        }
-        if (bpTime.minute > 0 || (bpTime.minute == 0 && bpTime.hour > 0)) {
-            str += bpTime.minute + ':';
-            if (bpTime.second < 10) {
-                str += '0';
-            }
-        }
-        str += bpTime.second;
-        if (parseInt(str) <= 0) {
-            str = "";
-        }
-        Message.PostAll({
-            setText: {
-                'id': '#bp-time',
-                'value': str
-            }
-        });
-    }
-
-    var resetBPTimer = function () {
-        clearInterval(bpTimer);
-        bpTimer = setInterval(function () {
-            bpTime.second--;
-            if (bpTime.second < 0) {
-                bpTime.minute--;
-                if (bpTime.minute < 0) {
-                    bpTime.hour--;
-                    if (bpTime.hour < 0) {
-                        setBP(currBP + 1, maxBP);
-                        stopBPTimer();
-                        setBPTime();
-                        Message.Notify('Your EP is full!', currBP + '/' + maxBP + ' EP', 'epNotifications');
-                        return;
-                    }
-                    bpTime.minute = 59;
-                }
-                if ((bpTime.minute === 19 || bpTime.minute === 39 || bpTime.minute === 59) && !(bpTime.hour === Math.floor((maxBP * 10 - 1) / 60) && bpTime.minute === (maxBP * 10 - 1) % 60)) {
-                    setBP(currBP + 1, maxBP);
-                }
-                bpTime.second = 59;
-            }
-            setBPTime();
-        }, 1000);
-    }
-    var stopBPTimer = function () {
-        bpTime.second = 0;
-        bpTime.minute = 0;
-        bpTime.hour = 0;
-        clearInterval(bpTimer);
-        bpTimer = false;
-        setBPTime();
+        bpTracker.setBpTime();
     }
 })();
---
+++ src/background/bpTracker.js
@@ -0,0 +1,196 @@
+class BpTracker {
+    constructor() {
+        this.currentBp = 0;
+        this.maxBp = 0;
+
+        this.bpTime = {
+            hour: 0,
+            minute: 0,
+            second: 0
+        };
+
+        this.bpTimer;
+        this.availableRaids = {};
+        this.currRaids = {};
+        this.raidBpCost = 0;
+    }
+
+    updateFromJson(status) {
+        this.setBp(status.bp, status.max_battle_point);
+        if (status.battle_point_remain.indexOf('00:00') === -1) {
+            var index = status.battle_point_remain.indexOf('h');
+            hour = this.bpTime.hour;
+            minute = this.bpTime.minute;
+            if (index !== -1) {
+                this.bpTime.hour = Number(status.battle_point_remain.substring(0, index));
+            } else {
+                this.bpTime.hour = 0;
+            }
+            if (status.battle_point_remain.indexOf('m') !== -1) {
+                this.bpTime.minute = Number(status.battle_point_remain.substring(index + 1, status.battle_point_remain.length - 1));
+            } else {
+                this.bpTime.minute = 0;
+            }
+            if (hour !== bpTime.hour || minute !== this.bpTime.minute) {
+                this.bpTime.second = 59;
+                this.setBpTime();
+                this.resetBpTimer();
+            }
+        } else {
+            this.stopBpTimer();
+        }
+    }
+
+    stageRaidFromJson(json) {
+        this.availableRaids = {};
+        var raid;
+        for (var i = 0; i < json.assist_raids_data.length; i++) {
+            raid = json.assist_raids_data[i];
+            this.availableRaids[raid.raid.id] = {
+                bp: raid.used_battle_point
+            };
+        }
+    }
+    stageRaidCodeFromJson(json) {
+        this.availableRaids = {};
+        this.availableRaids[json.raid.id] = {
+            bp: json.used_battle_point
+        };
+    }
+    spendBpOnStagedRaid(json, payload) {
+        if (json.result !== false) {
+            if (json.is_host === false) {
+                this.spendBp(parseInt(payload.select_bp));
+            }
+            this.currRaids[json.raid_id] = this.availableRaids[json.raid_id];
+        }
+        this.availableRaids = {};
+    }
+    getMaxBp() {
+        return this.maxBp;
+    }
+    spendBp(amount) {
+        var full = false;
+        if (this.currentBp >= this.maxBp) {
+            full = true;
+            this.setBp(this.currentBp - amount, this.maxBp);
+            if (this.currentBp < this.maxBp) {
+                amount = this.maxBp - this.currentBp;
+            } else {
+                return;
+            }
+        } else {
+            this.setBp(this.currentBp - amount, this.maxBp);
+        }
+        if (this.currentBp < this.maxBp) {
+            this.bpTime.minute += amount * 10 % 60;
+            if (this.bpTime.minute >= 60) {
+                this.bpTime.minute -= 60;
+                this.bpTime.hour++;
+            }
+            if (full) {
+                this.bpTime.minute--;
+                if (this.bpTime.minute < 0) {
+                    this.bpTime.minute += 60;
+                    this.bpTime.hour--;
+                }
+                this.bpTime.second = 59;
+            }
+            this.bpTime.hour += Math.floor(amount * 10 / 60);
+            this.setBpTime();
+            if (!this.bpTimer) {
+                this.resetBpTimer();
+            }
+        }
+    }
+    setBp(current, max) {
+        this.currentBp = parseInt(current);
+        this.maxBp = parseInt(max);
+        Message.PostAll({
+            setText: {
+                'id': '#bp-number',
+                'value': 'EP: ' + this.currentBp + '/' + this.maxBp
+            }
+        });
+        Message.PostAll({
+            setBar: {
+                'id': '#bp-bar',
+                'value': ((this.currentBp / this.maxBp) * 100) + '%'
+            }
+        });
+    }
+    addBp(amount) {
+        this.setBp(this.currentBp + amount, this.maxBp);
+        if (this.currentBp >= this.maxBp) {
+            this.stopBpTimer();
+        } else {
+            this.bpTime.minute -= amount * 10 % 60;
+            if (this.bpTime.minute < 0) {
+                this.bpTime.minute += 60;
+                this.bpTime.hour--;
+            }
+            this.bpTime.hour -= Math.floor(amount * 10 / 60);
+            this.setBpTime();
+        }
+    }
+    setBpTime() {
+        var output = "";
+        if (this.bpTime.hour > 0) {
+            output += this.bpTime.hour + ':';
+            if (this.bpTime.minute < 10) {
+                output += '0'
+            }
+        }
+        if (this.bpTime.minute > 0 || (this.bpTime.minute == 0 && this.bpTime.hour > 0)) {
+            output += this.bpTime.minute + ':';
+            if (this.bpTime.second < 10) {
+                output += '0';
+            }
+        }
+        output += this.bpTime.second;
+        if (parseInt(output) <= 0) {
+            output = "";
+        }
+        Message.PostAll({
+            setText: {
+                'id': '#bp-time',
+                'value': output
+            }
+        });
+    }
+
+    resetBpTimer() {
+        clearInterval(this.bpTimer);
+        this.bpTimer = setInterval(() => {
+            this.bpTime.second--;
+            if (this.bpTime.second < 0) {
+                this.bpTime.minute--;
+                if (this.bpTime.minute < 0) {
+                    this.bpTime.hour--;
+                    if (this.bpTime.hour < 0) {
+                        this.setBp(this.currentBp + 1, this.maxBp);
+                        this.stopBpTimer();
+                        this.setBpTime();
+                        Message.Notify('Your EP is full!', this.currentBp + '/' + this.maxBp + ' EP', 'epNotifications');
+                        return;
+                    }
+                    this.bpTime.minute = 59;
+                }
+                if ((this.bpTime.minute === 19 || this.bpTime.minute === 39 || this.bpTime.minute === 59) &&
+                    !(this.bpTime.hour === Math.floor((this.maxBp * 10 - 1) / 60) && this.bpTime.minute === (this.maxBp * 10 - 1) % 60)) {
+                    this.setBp(this.currentBp + 1, this.maxBp);
+                }
+                this.bpTime.second = 59;
+            }
+            this.setBpTime();
+        }, 1000);
+    }
+    stopBpTimer() {
+        this.bpTime.second = 0;
+        this.bpTime.minute = 0;
+        this.bpTime.hour = 0;
+        clearInterval(this.bpTimer);
+        this.bpTimer = false;
+        this.setBpTime();
+    }
+}
